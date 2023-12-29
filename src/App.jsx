f392b0f2c8e28a82f05e8b452d2c3b681818114c
Sankalp Sharma <sharma.sankalp97@gmail.com>
2023-12-29 10:34:01 +0530

    split components and added audio feedback

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import { Canvas } from '@react-three/fiber'
+import Experience from './Experience.jsx'
+import { useState, useEffect } from 'react';
+
+const audio = new Audio("./whoosh.wav");
+
+const App = function(){
+
+    const [startAudio, setStartAudio] = useState(false);
+    useEffect(() => {
+        if (startAudio) {
+          audio.play();
+          setStartAudio(false)
+          console.log("Play")
+        }
+      }, [startAudio]);
+
+
+    return (   
+        <>
+        <Canvas
+        onClick={()=>setStartAudio(true)}
+        className="r3f"
+        camera={ {
+            fov: 45,
+            near: 0.1,
+            far: 2000,
+            position: [ -3, 1.5, 4 ]
+        } }
+    >
+        <Experience />
+    </Canvas>
+        
+    </>)
+}
+
+export default App
